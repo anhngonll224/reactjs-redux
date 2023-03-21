3a2568c9384078bfb5ad03221b022fbdc14234d9
anhngonll224 <anhngonll224@gmail.com>
2023-03-21 17:20:37 +0700

    rectjs-redux

---
+++ react-redux/src/App.js
@@ -0,0 +1,67 @@
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/cart-actions';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvW8-_XATGdcEde9RDUIc0g0xfv1jUWwc",
+  authDomain: "reactjs1-5f83a.firebaseapp.com",
+  projectId: "reactjs1-5f83a",
+  storageBucket: "reactjs1-5f83a.appspot.com",
+  messagingSenderId: "368655744618",
+  appId: "1:368655744618:web:978a4636e90606e3011413"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+let isInitial = true;
+
+function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default App;
